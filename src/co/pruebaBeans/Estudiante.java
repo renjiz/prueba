aad2c569d59ee79e5b87a3ca2fb175e7167efa53
renjiz <renji_1308@hotmail.com>
2013-10-26 23:50:47 -0600

    cosas de la vida

---
+++ src/co/pruebaBeans/Estudiante.java
@@ -0,0 +1,41 @@
+package co.pruebaBeans;
+
+public class Estudiante {
+	
+	private String name;
+	private Integer edad;
+	private String mensaje;
+	
+	public void tonteria(String name, Integer edad){
+		this.name = name;
+		this.edad = edad;
+		
+		mensaje = "Estudiante se llama " + name + " y su edad es de " + edad + "."; 
+		mensaje = mensaje + " Las cosas de la vida!";
+		System.out.println(mensaje);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	
+	
+	
+
+}
