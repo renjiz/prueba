f5f133136d412a6fff5bc490ca42375eb4acef57
renjiz <renji_1308@hotmail.com>
2013-10-26 23:47:41 -0600

    Solo es de prueba

---
+++ src/co/pruebaBeans/Testing.java
@@ -0,0 +1,15 @@
+package co.pruebaBeans;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 Estudiante estu = new Estudiante();
+		 estu.tonteria("kike", 31);
+		
+		
+
+	}
+
+}
